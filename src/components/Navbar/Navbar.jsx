f971f6ee59e5c33ed4fc613c7b6b91ab69ddad60
Ashutosh24-go <266549407+Ashutosh24-go@users.noreply.github.com>
2026-07-12 21:54:08 +0530

    added mobile menu option

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,9 @@
 import './Navbar.css' 
+import { useState } from 'react';
 export function Navbar(){
+    const [menuOpen, setMenuOpen] = useState(false);
     return (
+        <>
         <nav>
         
             <div className="nav1">
@@ -13,12 +16,35 @@ export function Navbar(){
             <a href="#aboutme" className="navigation">04. ABOUT </a>
             <a href="#" className="navigation">05. CONTACT</a>
         
-                <button className="homebutton" id="menudriven"><i className="ri-menu-unfold-4-line"></i> 
+                <button className="homebutton" id="menudriven"><i className="ri-menu-unfold-4-line"  onClick={()=>{
+                    setMenuOpen(!menuOpen)
+                }}></i> 
                     
                 </button>
                 
             </div>
         
     </nav>
-    )
+        {
+            menuOpen && (
+               
+        
+          
+            
+            <div className="mobilenav">
+            <a href="#home" className="navigation" onClick={() => setMenuOpen(false)} >01. HOME</a>
+            <a href="#work" className="navigation " onClick={() => setMenuOpen(false)} >02. WORK</a>
+            <a href="#skills" className="navigation" onClick={() => setMenuOpen(false)}>03. SKILLS</a>
+            <a href="#aboutme" className="navigation" onClick={() => setMenuOpen(false)}>04. ABOUT </a>
+            <a href="#" className="navigation"onClick={() => setMenuOpen(false)} >05. CONTACT</a>
+        
+                
+                
+            </div>
+        
+    
+            )
+        }
+        </>
+    );
 }
